fb09c75930707052bd101242a7651ef80a2de40e
Dennis Cahillane <dennis@thirtymadison.com>
2021-12-31 21:01:36 -0600

    fix footer

--- src/App.js
+++ src/App.js
@@ -1,16 +1,16 @@
 import './App.css';
 import Board from './Board';
+import Footer from './Footer';
 
 // 14x14 board
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <p>
-          Align Polyminoes
-        </p>
-        <Board></Board>
+      <header className='App-header'>
+        <p>Align Polyminoes</p>
+        <Board />
       </header>
+      <Footer />
     </div>
   );
 }
---
+++ src/Footer.jsx
@@ -0,0 +1,16 @@
+const Footer = () => {
+  return <div className="footer" style={{ 
+    borderTop: '1px solid #e8e8e8',
+    position: 'fixed',
+    left: 0,
+    bottom: 0,
+    width: '100%',
+    backgroundColor: 'white',
+    textAlign: 'center',}}>
+      Align Polyminoes © 2021 - Built with ❤️ in NYC by&nbsp;
+      <a href="https://twitter.com/DennisLibre">Dennis</a>.&nbsp;
+      <a href="https://github.com/LinuxFan2718/align-polyominoes">Source code</a>.
+  </div>;
+};
+
+export default Footer;
